3c2998a50b95c35f1951d45d8bd67bed3447f43e
acamq <al.charmquark@gmail.com>
2025-05-14 08:44:03 -0600

    added getPlayers, checkWin

--- Game.java
+++ Game.java
@@ -1,16 +1,47 @@
 import java.util.Scanner;
 public class Game {
-
+    private Player[] players;
     public static String getInput(String toPrint){
         Scanner scanner = new Scanner(System.in);
         System.out.println(toPrint);
         return scanner.nextLine();
     }
 
-    public static void main(String[] args){
-        int numPlayers = StringToInt.stringToInt(getInput("Number of players?"));
+    public void getPlayers(){
+        int numPlayers = StringToInt.stringToInt("Number of players?");
+        players = new Player[numPlayers];
         for (int i = 0; i<numPlayers; i++){
-             getInput("Name of player "+ (i+1));
+            String name = getInput("Name of player "+ (i+1));
+            players[i] = new Player(name, i);
         }
+    }
+    public boolean checkWin(){
+        int numOut = 0;
+        for (int i = 0; i < players.length; i++){
+            if (players[i].isOut()){
+                numOut++;
+            }
+        }
+        if (numOut == players.length -1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public void Play(){
+        getPlayers();
+        boolean win = false;
+        while (!win) {
+            for (int i = 0; i < players.length; i++){
+                //takeAction();
+            }
+            win = checkWin();
+        }
+    }
+
+    public static void main(String[] args){
+        Game game = new Game();
+        game.Play();
     }
 }
