9dec65bb2ae138f8ac2551649b993f1e57af3125
Your Name <you@example.com>
2025-10-29 12:49:29 -0600

    Clean up formatting

--- ActionsPrinter.java
+++ ActionsPrinter.java
@@ -2,27 +2,24 @@ public class ActionsPrinter {
     /**String methods to avoid clutter*/
     public static int numActions = 9;
     public static String welcomeMessage(){
-        String welcomeMessage =
-                "Welcome to Coup! You can read the rules here. https://www.qugs.org/rules/r131357.pdf.\n" +
-                        "Please use your keyboard as prompted to take actions. Unless otherwise specified, decline an action by not inputting anything and pressing enter. Have fun!";
-        return welcomeMessage;
+        return "Welcome to Coup! You can read the rules here. https://www.qugs.org/rules/r131357.pdf.\n" +
+                "Please use your keyboard as prompted to take actions. Unless otherwise specified, decline an action by not inputting anything and pressing enter. Have fun!";
     }
     public static String instructions(){
-        String instructions =
-                "You can read the rules here. https://www.qugs.org/rules/r131357.pdf.\n" +
-                        "Please use your keyboard as prompted to take actions. Unless otherwise specified, decline an action by not inputting anything and pressing enter.";
-        return instructions;
+        return "You can read the rules here. https://www.qugs.org/rules/r131357.pdf.\n" +
+                "Please use your keyboard as prompted to take actions. Unless otherwise specified, decline an action by not inputting anything and pressing enter.";
     }
     public static String getActions(){
-        String actions = "1. Income\n"
-                + "2. Foreign Aid\n"
-                + "3. Coup (needs 7 tokens)\n"
-                + "4. Tax\n"
-                + "5. Assassinate\n"
-                + "6. Steal\n"
-                + "7. Exchange\n"
-                + "8. View your influences\n"
-                + "9. View instructions and rules";
+        String actions = """
+                1. Income
+                2. Foreign Aid
+                3. Coup (needs 7 tokens)
+                4. Tax
+                5. Assassinate
+                6. Steal
+                7. Exchange
+                8. View your influences
+                9. View instructions and rules""";
         return actions;
     }
 }
